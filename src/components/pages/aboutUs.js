57800d0b131d2693526f21c886fbb9b0e962e777
carlos.hinestrozap <https://github.com/CarlosAndress/Project_Frontend.git>
2022-04-26 23:44:37 -0500

    new_code

--- src/components/pages/aboutUs.js
+++ src/components/pages/aboutUs.js
@@ -1,5 +1,15 @@
-const aboutUS = () =>(
-    <h1>About carlos</h1>
+/* eslint-disable jsx-a11y/iframe-has-title */
+const aboutUS = () => (
+    <>
+        <h1>Created by Carlos Andrés SH1FT3R</h1>
+        <div className="ed-video">
+        <iframe src="https://www.youtube.com/embed/Pzmf1GoRRXA" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen></iframe>
+        </div>
+    </>
 )
 
 export default aboutUS;
